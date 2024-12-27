a878ef4bd41c9035ec67e87b137e6d8bc2613acc
val213 <1042594899@qq.com>
2024-12-28 00:32:07 +0800

    基本框架搭建完成

--- src/main/java/com/example/iredms/IreDmsApplication.java
+++ src/main/java/com/example/iredms/IreDmsApplication.java
@@ -2,8 +2,13 @@ package com.example.iredms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
+@PropertySource("file:.env")
+@ComponentScan(basePackages = {"com.huawei.innovation", "com.example.iredms"})
 public class IreDmsApplication {
 
     public static void main(String[] args) {
