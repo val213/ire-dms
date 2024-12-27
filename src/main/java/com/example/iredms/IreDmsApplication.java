fd59434fd5f9641670051e9415eb610ed6c3b28a
val213 <1042594899@qq.com>
2024-12-27 16:38:24 +0800

    updates

---
+++ src/main/java/com/example/iredms/IreDmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.iredms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IreDmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IreDmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iredms/IreDmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iredms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IreDmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
