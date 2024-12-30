19b0138aea7965976d60fce215bba6d402a56ac8
Arella-Ovo <yuli21305860@gmail.com>
2024-12-30 17:01:26 +0800

    UserController,UserService,UserServicelmpl（创，查，删）

--- src/main/java/com/example/iredms/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/iredms/service/impl/UserServiceImpl.java
@@ -1,4 +1,42 @@
 package com.example.iredms.service.impl;
 
-public class UserServiceImpl {
+import com.example.iredms.service.ProductService;
+import com.example.iredms.service.UserService;
+import com.huawei.innovation.rdm.coresdk.basic.dto.PersistObjectIdModifierDTO;
+import com.huawei.innovation.rdm.coresdk.basic.vo.QueryRequestVo;
+import com.huawei.innovation.rdm.coresdk.basic.vo.RDMPageVO;
+import com.huawei.innovation.rdm.intelligentrobotengineering.delegator.UserDelegator;
+import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserCreateDTO;
+import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserQueryViewDTO;
+import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserViewDTO;
+import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserUpdateDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDelegator userDelegator;
+
+    @Override
+    public UserViewDTO create(@RequestBody UserCreateDTO userCreateDTO) {
+        return userDelegator.create(userCreateDTO);
+    }
+
+    @Override
+    public List<UserQueryViewDTO> query(@RequestBody QueryRequestVo queryRequestVo) {
+        RDMPageVO pageVO = new RDMPageVO(1, 10);
+        return userDelegator.query(queryRequestVo, pageVO);
+    }
+
+    @Override
+    public int delete(PersistObjectIdModifierDTO userDeleteRequestDTO) {
+        return userDelegator.delete(userDeleteRequestDTO);
+    }
 }
