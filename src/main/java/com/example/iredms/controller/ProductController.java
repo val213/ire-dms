214151eeff1ab7698142744b14a55af4e2915c80
archertzy <134219290+archertzy@users.noreply.github.com>
2024-12-31 02:10:22 +0800

    Squashed commit of the following:
    commit 4d03a201e40bb37dae751429559c8b363cd29c39
    Merge: c2db940 e8180dd
    Author: Val213 <112376067+val213@users.noreply.github.com>
    Date:   Tue Dec 31 01:46:59 2024 +0800
    
        Merge pull request #7 from val213/product
    
        updates:更新完善product的 create 和 query 满足需求
    
    commit e8180ddb0ec259ce42d9d7917115b3c5c879e8a8
    Author: val213 <1042594899@qq.com>
    Date:   Tue Dec 31 01:46:02 2024 +0800
    
        updates:更新完善product的 create 和 query 满足需求
    
    commit c2db940ee84bd73449b5197a537d6befbb42eac1
    Merge: 71f74f7 19b0138
    Author: Val213 <112376067+val213@users.noreply.github.com>
    Date:   Mon Dec 30 23:33:04 2024 +0800
    
        Merge pull request #6 from Arella-Ovo/main
    
        User基础接口定义
    
    commit 19b0138aea7965976d60fce215bba6d402a56ac8
    Author: Arella-Ovo <yuli21305860@gmail.com>
    Date:   Mon Dec 30 17:01:26 2024 +0800
    
        UserController,UserService,UserServicelmpl（创，查，删）
    
    commit 31a2857388c6987137153487c3d6997076c7b927
    Author: val213 <1042594899@qq.com>
    Date:   Sun Dec 29 17:07:52 2024 +0800
    
        补充主要的接口，但是还没做条件限定
    
    commit 71f74f78ec04858befe6c64dae6a3c9c1849fa81
    Merge: 93b7b6d d3e358d
    Author: Val213 <112376067+val213@users.noreply.github.com>
    Date:   Sun Dec 29 15:26:27 2024 +0800
    
        Merge pull request #5 from val213/product
    
        统一返回类型并添加部分 Product 接口
    
    commit d3e358d658edab4ed57fd8b0953148c0403abcdd
    Author: val213 <1042594899@qq.com>
    Date:   Sun Dec 29 15:24:14 2024 +0800
    
        updates and fix .env
    
    commit ed556162420732558694a3127d5dca70de8316f4
    Author: val213 <1042594899@qq.com>
    Date:   Sun Dec 29 15:06:46 2024 +0800
    
        updates
    
    commit a114bebdf41daed41a54b951221ba31424d519ff
    Author: val213 <1042594899@qq.com>
    Date:   Sun Dec 29 11:22:15 2024 +0800
    
        updates some api for product and impl the baserespone utils
    
    commit 1a29588aa24344c3268812ee6a66755cb362a015
    Author: val213 <1042594899@qq.com>
    Date:   Sat Dec 28 22:38:27 2024 +0800
    
        init the controller apis

--- src/main/java/com/example/iredms/controller/ProductController.java
+++ src/main/java/com/example/iredms/controller/ProductController.java
@@ -2,32 +2,45 @@ package com.example.iredms.controller;
 
 import com.example.iredms.common.BaseResponse;
 import com.example.iredms.common.ResultUtils;
+import com.example.iredms.dto.ProductQueryDTO;
 import com.example.iredms.service.ProductService;
+import com.huawei.innovation.rdm.coresdk.basic.dto.PersistObjectIdDecryptDTO;
+import com.huawei.innovation.rdm.coresdk.basic.dto.PersistObjectIdModifierDTO;
+import com.huawei.innovation.rdm.coresdk.basic.enums.ConditionType;
+import com.huawei.innovation.rdm.coresdk.basic.vo.DeleteByConditionVo;
+import com.huawei.innovation.rdm.coresdk.basic.vo.UpdateByConditionVo;
+import com.huawei.innovation.rdm.intelligentrobotengineering.bean.relation.ProductBlueprintLink;
 import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.ProductUpdateDTO;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.huawei.innovation.rdm.intelligentrobotengineering.dto.relation.ProductBlueprintLinkCreateDTO;
+import com.huawei.innovation.rdm.intelligentrobotengineering.dto.relation.ProductBlueprintLinkViewDTO;
+import com.huawei.innovation.rdm.intelligentrobotengineering.dto.relation.ProductPartLinkCreateDTO;
+import com.huawei.innovation.rdm.intelligentrobotengineering.dto.relation.ProductPartLinkViewDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import com.huawei.innovation.rdm.coresdk.basic.vo.QueryRequestVo;
 import com.huawei.innovation.rdm.coresdk.basic.vo.RDMPageVO;
-import com.huawei.innovation.rdm.intelligentrobotengineering.delegator.ProductDelegator;
 import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.ProductCreateDTO;
 import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.ProductQueryViewDTO;
 import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.ProductViewDTO;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+
+import static org.reflections.Reflections.log;
+
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
-    ProductService productService;
+    @Autowired
+    private ProductService productService;
     /**
      * 查询产品：可按编号精确查询，按名称模糊查询，展示产品编号、名称、基
      * 本信息、负责人、产品阶段等字段
      */
     @RequestMapping("/query")
-    public BaseResponse<List<ProductQueryViewDTO>> query(@RequestBody QueryRequestVo queryRequestVo) {
-        RDMPageVO pageVO = new RDMPageVO(1, 10);
-        return ResultUtils.success(productService.query(queryRequestVo));
+    public BaseResponse<List<ProductViewDTO>> query(@RequestBody ProductQueryDTO productQueryDTO) {
+        log.info("query：{}", productQueryDTO);
+        return ResultUtils.success(productService.query(productQueryDTO));
     }
 
     /**
@@ -36,44 +49,71 @@ public class ProductController {
      * 产品阶段默认为初始阶段
      */
     @RequestMapping("/create")
-    public BaseResponse<ProductViewDTO> create(@RequestBody ProductCreateDTO productCreateDTO) {
-        return ResultUtils.success(productService.create(productCreateDTO));
+    public BaseResponse<Boolean> create(@RequestBody ProductViewDTO productViewDTO) {
+        return ResultUtils.success(productService.create(productViewDTO));
     }
 
-//    /**
-//     * 修改产品：点击“修改”按钮，可编辑该产品信息：名称、基本信息、负责人、
-//     * 产品阶段字段。（产品阶段只能由管理员修改）
-//     */
-//    @RequestMapping("/update")
-//    public BaseResponse<ProductUpdateDTO> update() {
-//        ResultUtils.success(productService.update());
-//    }
-//
-//    /**
-//     * 删除产品：点击“删除”按钮，可删除该产品
-//     * 产品详情：点击“查看详情”按钮，可查看该产品信息：产品编号、名称、基本信
-//     * 息、负责人、产品阶段、关联蓝图和部件编号。
-//     */
-//    @RequestMapping("/delete")
-//    public String delete() {
-//        ResultUtils.success(productService.delete());
-//    }
-//
-//    /**
-//     * 编辑并展示关联蓝图：可创建删除该产品与蓝图关系（形式自由）
-//     * 注意：仅当产品处于概念化和设计阶段时，方可编辑
-//     */
-//    @RequestMapping("/blueprint")
-//    public String blueprint() {
-//        ResultUtils.success(productService.blueprint());
-//    }
-//
-//    /**
-//     * 编辑并展示关联部件：可创建删除该产品与部件关系（形式自由）
-//     * 注意：仅当产品处于原型开发阶段时，方可编辑
-//     */
-//    @RequestMapping("/component")
-//    public String component() {
-//        ResultUtils.success(productService.component());
-//    }
+    /**
+     * 修改产品：点击“修改”按钮，可编辑该产品信息：名称、基本信息、负责人、
+     * 产品阶段字段。（产品阶段只能由管理员修改）
+     */
+    @RequestMapping("/update")
+    public BaseResponse<ProductViewDTO> update(@RequestBody UpdateByConditionVo<ProductUpdateDTO> productUpdateDTO) {
+        return ResultUtils.success(productService.update(productUpdateDTO));
+    }
+
+    /**
+     * 删除产品：点击“删除”按钮，可删除该产品
+     * 注意：仅当部件未被任何产品调用时，方可删除
+     */
+    @RequestMapping("/delete/{id}")
+    public BaseResponse<Integer> delete(@PathVariable String id) {
+        DeleteByConditionVo deleteByConditionVo = new DeleteByConditionVo();
+        QueryRequestVo queryRequestVo = new QueryRequestVo();
+        queryRequestVo.addCondition("id",ConditionType.EQUAL,id);
+        deleteByConditionVo.setCondition(queryRequestVo);
+        return ResultUtils.success(productService.delete(deleteByConditionVo));
+    }
+    /**
+     * 产品详情：点击“查看详情”按钮，可查看该产品信息：产品编号、名称、基本信
+     * 息、负责人、产品阶段、关联蓝图和部件编号。
+     */
+    @RequestMapping(value = "/detail", method = RequestMethod.POST)
+    public BaseResponse<ProductViewDTO> detail(@RequestBody PersistObjectIdDecryptDTO productDetailRequestDTO) {
+        return ResultUtils.success(productService.detail(productDetailRequestDTO));
+    }
+    /**
+     * 编辑并展示关联蓝图：可创建删除该产品与蓝图关系（形式自由）
+     * 注意：仅当产品处于概念化和设计阶段时，方可编辑
+     */
+    @RequestMapping("/blueprint/create")
+    public BaseResponse<Boolean> createProductBlueprintLink(@RequestBody ProductBlueprintLinkViewDTO productBlueprintLinkViewDTO) {
+        return ResultUtils.success(productService.createProductBlueprintLink(productBlueprintLinkViewDTO));
+    }
+    @RequestMapping("/blueprint/delete/{id}")
+    public BaseResponse<Integer> blueprintDelete(@PathVariable String id) {
+        DeleteByConditionVo deleteByConditionVo = new DeleteByConditionVo();
+        QueryRequestVo queryRequestVo = new QueryRequestVo();
+        queryRequestVo.addCondition("id",ConditionType.EQUAL,id);
+        deleteByConditionVo.setCondition(queryRequestVo);
+        return ResultUtils.success(productService.deleteProductBlueprintLink(deleteByConditionVo));
+    }
+
+    /**
+     * 编辑并展示关联部件：可创建删除该产品与部件关系（形式自由）
+     * 注意：仅当产品处于原型开发阶段时，方可编辑
+     */
+    @RequestMapping("/part/create")
+    public BaseResponse<Boolean> createProductPartLink(@RequestBody ProductPartLinkViewDTO productPartViewRequestDTO) {
+        return ResultUtils.success(productService.createProductPartLink(productPartViewRequestDTO));
+    }
+
+    @RequestMapping("/part/delete/{id}")
+    public BaseResponse<Integer> partDelete(@PathVariable String id) {
+        DeleteByConditionVo deleteByConditionVo = new DeleteByConditionVo();
+        QueryRequestVo queryRequestVo = new QueryRequestVo();
+        queryRequestVo.addCondition("id",ConditionType.EQUAL,id);
+        deleteByConditionVo.setCondition(queryRequestVo);
+        return ResultUtils.success(productService.deleteProductPartLink(deleteByConditionVo));
+    }
 }
