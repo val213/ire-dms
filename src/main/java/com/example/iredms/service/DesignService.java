caea6c1ef78c62f27ea2851c6793f618bb4940c1
archertzy <134219290+archertzy@users.noreply.github.com>
2024-12-31 20:02:14 +0800

    design

--- src/main/java/com/example/iredms/service/DesignService.java
+++ src/main/java/com/example/iredms/service/DesignService.java
@@ -1,4 +1,13 @@
 package com.example.iredms.service;
 
+import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.DesignBlueprintViewDTO;
+
+import java.util.List;
+
 public interface DesignService {
+    public DesignBlueprintViewDTO createDesignBlueprint(DesignBlueprintViewDTO designBlueprintViewDTO);
+    public DesignBlueprintViewDTO updateDesignBlueprint(DesignBlueprintViewDTO designBlueprintViewDTO);
+    public int deleteDesignBlueprint(DesignBlueprintViewDTO designBlueprintViewDTO);
+    public List<DesignBlueprintViewDTO> queryDesignBlueprints(DesignBlueprintViewDTO designBlueprintViewDTO);
+
 }
--- src/main/java/com/example/iredms/service/impl/DesignServiceImpl.java
+++ src/main/java/com/example/iredms/service/impl/DesignServiceImpl.java
@@ -1,4 +1,53 @@
 package com.example.iredms.service.impl;
 
-public class DesignServiceImpl {
+import com.example.iredms.service.DesignService;
+import com.huawei.innovation.rdm.coresdk.basic.enums.ConditionType;
+import com.huawei.innovation.rdm.coresdk.basic.vo.DeleteByConditionVo;
+import com.huawei.innovation.rdm.coresdk.basic.vo.QueryRequestVo;
+import com.huawei.innovation.rdm.intelligentrobotengineering.delegator.DesignBlueprintDelegator;
+import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.DesignBlueprintCreateDTO;
+import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.DesignBlueprintViewDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DesignServiceImpl implements DesignService {
+    @Autowired
+    private DesignBlueprintDelegator designBlueprintDelegator;
+
+    public DesignServiceImpl(DesignBlueprintDelegator designBlueprintDelegator) {
+        this.designBlueprintDelegator = designBlueprintDelegator;
+    }
+
+    @Override
+    public DesignBlueprintViewDTO createDesignBlueprint(DesignBlueprintViewDTO designBlueprintViewDTO) {
+        DesignBlueprintCreateDTO designBlueprint = new DesignBlueprintCreateDTO();
+        if (designBlueprint.getBuleprintDescription() != null && !designBlueprint.getBuleprintDescription().isEmpty()) {
+            designBlueprint.setBuleprintDescription(designBlueprint.getBuleprintDescription());
+        }
+        return designBlueprintDelegator.create(designBlueprint);
+    }
+
+    @Override
+    public DesignBlueprintViewDTO updateDesignBlueprint(DesignBlueprintViewDTO designBlueprintViewDTO) {
+        return null;
+    }
+
+    @Override
+    public int deleteDesignBlueprint(DesignBlueprintViewDTO designBlueprintViewDTO) {
+        if (designBlueprintViewDTO.getId() != null) {
+            QueryRequestVo queryRequestVo = new QueryRequestVo();
+            queryRequestVo.addCondition("id", ConditionType.EQUAL,designBlueprintViewDTO.getId());
+            DeleteByConditionVo deleteByConditionVo = new DeleteByConditionVo();
+            deleteByConditionVo.setCondition(queryRequestVo);
+            return designBlueprintDelegator.deleteByCondition(deleteByConditionVo);
+        } else return 0;
+    }
+
+    @Override
+    public List<DesignBlueprintViewDTO> queryDesignBlueprints(DesignBlueprintViewDTO designBlueprintViewDTO) {
+        return List;
+    }
 }
