8917aff5fe619268e44cc77ff15ec118d5ffc451
Arella-Ovo <yuli21305860@gmail.com>
2024-12-31 15:44:07 +0800

    UserController,UserService,UserServicelmpl

--- src/main/java/com/example/iredms/controller/UserController.java
+++ src/main/java/com/example/iredms/controller/UserController.java
@@ -12,6 +12,7 @@ import com.huawei.innovation.rdm.coresdk.basic.vo.RDMPageVO;
 import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserCreateDTO;
 import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserQueryViewDTO;
 import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserViewDTO;
+import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserUpdateDTO;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
@@ -23,20 +24,55 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /**
+     * 查询用户：可按用户名精确查询，展示注册日期、用户名、电话号码、权限等字段
+     */
     @RequestMapping("/query")
     public BaseResponse<List<UserQueryViewDTO>> query(@RequestBody QueryRequestVo queryRequestVo) {
         RDMPageVO pageVO = new RDMPageVO(1, 10);
         return ResultUtils.success(userService.query(queryRequestVo));
     }
 
-    @RequestMapping("/create")
-    public BaseResponse<UserViewDTO> create(@RequestBody UserCreateDTO userCreateDTO) {
-        return ResultUtils.success(userService.create(userCreateDTO));
+    /**
+     //     * 修改用户：点击“修改”按钮，可编辑该用户信息：用户名、电话号码、权限字段。
+     //     * （只能由管理员修改）
+     //     */
+    @RequestMapping("/update")
+    public BaseResponse<UserViewDTO> update_byAdmin(@RequestBody UserUpdateDTO user) {
+
+        UserUpdateDTO u = new UserUpdateDTO();
+        if (user.getName()!= null && !user.getName().isEmpty()) {
+            u.setName(user.getName());
+        }
+        if (user.getPhone()!= null && !user.getPhone().isEmpty()) {
+            u.setPhone(user.getPhone());
+        }
+        if (user.getAuthority()!= null) {
+            u.setAuthority(user.getAuthority());
+        }
+
+        return ResultUtils.success(userService.update(user));
     }
 
-    @RequestMapping("/delete")
-    public BaseResponse<Integer> delete(PersistObjectIdModifierDTO userDeleteRequestDTO) {
-        return ResultUtils.success(userService.delete(userDeleteRequestDTO));
+    /**
+     //     * 修改个人信息：点击“修改信息”，可编辑该用户信息：用户名、密码、电话号码字段。
+     //     * （用户自己修改）
+     //     */
+    @RequestMapping("/update")
+    public BaseResponse<UserViewDTO> update(@RequestBody UserUpdateDTO user) {
+
+        UserUpdateDTO u = new UserUpdateDTO();
+        if (user.getName()!= null && !user.getName().isEmpty()) {
+            u.setName(user.getName());
+        }
+        if (user.getUserPassword()!= null && !user.getUserPassword().isEmpty()) {
+            u.setUserPassword(user.getUserPassword());
+        }
+        if (user.getPhone()!= null && !user.getPhone().isEmpty()) {
+            u.setPhone(user.getPhone());
+        }
+
+        return ResultUtils.success(userService.update(user));
     }
 
 }
--- src/main/java/com/example/iredms/service/UserService.java
+++ src/main/java/com/example/iredms/service/UserService.java
@@ -1,19 +1,16 @@
 package com.example.iredms.service;
 
-import com.huawei.innovation.rdm.coresdk.basic.dto.PersistObjectIdModifierDTO;
 import com.huawei.innovation.rdm.coresdk.basic.vo.QueryRequestVo;
-import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserCreateDTO;
 import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserQueryViewDTO;
 import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserViewDTO;
-//import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserUpdateDTO;
+import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserUpdateDTO;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 public interface UserService {
 
-    UserViewDTO create(@RequestBody UserCreateDTO userCreateDTO);
     List<UserQueryViewDTO> query(@RequestBody QueryRequestVo queryRequestVo);
-    int delete(@RequestBody PersistObjectIdModifierDTO productDeleteRequestDTO);
+    UserViewDTO update(@RequestBody UserUpdateDTO userUpdateDTO);
 
 }
