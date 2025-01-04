51db37bed1cd11edee7763c0b69e595bf97be50a
Arella-Ovo <yuli21305860@gmail.com>
2025-01-04 17:29:50 +0800

    修改查询返回值，给更新操作添加参数ID

--- src/main/java/com/example/iredms/service/UserService.java
+++ src/main/java/com/example/iredms/service/UserService.java
@@ -1,12 +1,12 @@
 package com.example.iredms.service;
 
-//import com.huawei.innovation.rdm.coresdk.basic.vo.
 import com.huawei.innovation.rdm.coresdk.basic.vo.QueryRequestVo;
 import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserQueryViewDTO;
 import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserViewDTO;
 import com.huawei.innovation.rdm.intelligentrobotengineering.dto.entity.UserUpdateDTO;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,8 +15,8 @@ public interface UserService {
     Boolean create(@RequestBody UserViewDTO userViewDTO);
     Boolean login(@PathVariable String name, @PathVariable String userPassword);
 
-    List<UserQueryViewDTO> query(@PathVariable String name);
-    Boolean update_a(@RequestBody UserUpdateDTO userUpdateDTO);
-    Boolean update_s(@RequestBody UserUpdateDTO userUpdateDTO);
+    List<UserViewDTO> query(@PathVariable String name);
+    Boolean update_a(@RequestParam Long id, @RequestBody UserUpdateDTO userUpdateDTO);
+    Boolean update_s(@RequestParam Long id, @RequestBody UserUpdateDTO userUpdateDTO);
 
 }
